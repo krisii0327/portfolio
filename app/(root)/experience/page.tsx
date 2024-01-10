8064ca54da4e03a718d8af6334465e769c1f56d5
Kristóf Kövesi <110851420+krisii0327@users.noreply.github.com>
2024-01-10 21:05:47 +0100

    init

--- app/(root)/experience/page.tsx
+++ app/(root)/experience/page.tsx
@@ -12,6 +12,35 @@ const page = () => {
     return (
       <section className='max-width tablet:mt-12 tablet:mb-12 mt-8 mb-8'>
 
+        {/* about */}
+        <div>
+          <SectionHeading title={'About me'} subtitle=''/>
+
+          <div className='flex flex-col gap-2 font-semibold text-slate-600 text-base'>
+            <span>
+              As a person who is in the beginning of his career and also a student aswell, I have a big motivation for getting experiences in the future.
+            </span>
+            <span>
+              After the Bachelor's degree, I started to work as a System Engineer. This workposition is very diversified, I had chance to manage a lot of VM, Databases and Systems. I also had chance to understand robots and to configure them. I met with a lot of different foreign worker.
+            </span>
+            <span>
+              I started to learn about Cyber Security at University of Óbuda, Budapest. I finished the first half year successfully. Cyber security will be very useful in the future as the world evolves. In this half year, we went deeper at the cryptograpy tecniques, databases and cloud computing. And we also took over what we learned during our previous studies. In the next half year, we will learn about the SOC tecniques. We will get the answer how we can defend our companies informations from the hackers. How we can recognize a cyber security attack as soon as possible.
+            </span>
+            <span>
+              In my free time I started to get deeper at programming. Firstly I met with programming at University of Pécs. I also got some knowledge about the machine codes for example Bash, Powershell and AWK languages too. But the program code was more understandable for me. I learned the basics about Python, C# and HTML (CSS).
+              After these programming languages I started to learn Javascript to develop webpages. I am interested in both of the Backend and Frontend techniques. I picked one of the most used framework, NextJS (React). I think I picked very well.
+              Because of it, I got better knowledge about backend Javascript programming, Tailwind style methods, about the MongoDB database, ExpressJS, NodeJS. I also learned about the API calls in web.
+            </span>
+            <span>
+              I started to make my own projects, to get better in both of the sides in web developing.
+            </span>
+            <span>
+              My main goal at the moment is to get better in the IT world. To be a relevant person in IT.
+            </span>
+          </div>
+        </div>
+
+
         {/* carrer */}
         <div>
           <SectionHeading title={'My work experiences'} subtitle={'places where I learned a lot'}/>
